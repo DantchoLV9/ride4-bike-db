e3fccd1c90857cb85b38195b72e64c272c64717a
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-09-25 01:38:21 +0100

    Sort functions completed

--- components/BikeGallery.js
+++ components/BikeGallery.js
@@ -1,11 +1,15 @@
 import BikeCard from "./BikeCard";
 
-const BikeGallery = ({ displayData }) => {
+const BikeGallery = ({ displayData, sortKey }) => {
 	console.log(displayData);
 	return (
 		<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full mb-16">
 			{displayData.map((bike) => (
-				<BikeCard key={`${bike.make}-${bike.model}`} bikeData={bike} />
+				<BikeCard
+					key={`${bike.make}-${bike.model}`}
+					bikeData={bike}
+					sortKey={sortKey}
+				/>
 			))}
 		</div>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import BikeCard from "../components/BikeCard";
 import Layout from "../components/Layout";
 import { getAllBikes } from "../lib/bikes";
 import Head from "next/head";
@@ -9,19 +8,96 @@ export default function Home({ data }) {
 	const allBikes = [];
 	data.forEach((model) => model.bikes.forEach((bike) => allBikes.push(bike)));
 	const [displayData, setDisplayData] = useState(allBikes);
-
+	const [sortingOrder, setSortingOrder] = useState("accending");
+	const [sortKey, setSortKey] = useState("make");
 	const SortByInputHandler = (e) => {
-		if (e.target.value === "year") {
-			setDisplayData(sortByYear(allBikes));
-		} else if (e.target.value === "make") {
-			setDisplayData(allBikes);
+		switch (e.target.value) {
+			case "make":
+				setDisplayData(allBikes);
+				setSortKey("make");
+				break;
+			case "year":
+				setDisplayData(sortBikes(allBikes, "year"));
+				setSortKey("year");
+				break;
+			case "displacement":
+				setDisplayData(sortBikes(allBikes, "displacement"));
+				setSortKey("displacement");
+				break;
+			case "power":
+				setDisplayData(sortBikes(allBikes, "power", true));
+				setSortKey("power");
+				break;
+			case "torque":
+				setDisplayData(sortBikes(allBikes, "torque", true));
+				setSortKey("torque");
+				break;
+			case "weight":
+				setDisplayData(sortBikes(allBikes, "weight", true));
+				setSortKey("weight");
+				break;
+			case "price":
+				setDisplayData(sortBikes(allBikes, "price"));
+				setSortKey("price");
+				break;
+			case "rating":
+				setDisplayData(sortBikes(allBikes, "rating"));
+				setSortKey("rating");
+				break;
+			case "acceleration":
+				setDisplayData(sortBikes(allBikes, "acceleration"));
+				setSortKey("acceleration");
+				break;
+			case "top-speed":
+				setDisplayData(sortBikes(allBikes, "top-speed"));
+				setSortKey("top-speed");
+				break;
+			case "handling":
+				setDisplayData(sortBikes(allBikes, "handling"));
+				setSortKey("handling");
+				break;
+			case "braking-power":
+				setDisplayData(sortBikes(allBikes, "braking-power"));
+				setSortKey("braking-power");
+				break;
+			default:
+				setDisplayData(allBikes);
+				setSortKey("make");
+				break;
 		}
 	};
-	const sortByYear = (data) => {
+	const SortOrderInputHandler = (e) => {
+		if (e.target.value === "accending") {
+			if (sortingOrder === "descending") {
+				let data = [...displayData];
+				setDisplayData(data.reverse());
+				setSortingOrder("accending");
+			}
+		} else if (e.target.value === "descending") {
+			if (sortingOrder === "accending") {
+				let data = [...displayData];
+				setDisplayData(data.reverse());
+				setSortingOrder("descending");
+			}
+		}
+	};
+	const sortBikes = (data, key, possibleNoData = false) => {
 		let arr = [...data];
+		let noPowerDataArr = [];
+		if (possibleNoData) {
+			arr.forEach((item, index) => {
+				if (item.power === "--") {
+					noPowerDataArr.push(item);
+					arr.splice(index, 1);
+				}
+			});
+		}
 		arr.sort((a, b) => {
-			return b.year > a.year ? 1 : b.year < a.year ? -1 : 0;
+			return b[key] > a[key] ? 1 : b[key] < a[key] ? -1 : 0;
 		});
+		if (possibleNoData) {
+			arr.push(...noPowerDataArr);
+		}
 		return arr;
 	};
 	return (
@@ -29,16 +105,30 @@ export default function Home({ data }) {
 			<Head>
 				<title>Ride 4 - Bikes</title>
 			</Head>
-			<div>
+			<div className="mb-6">
 				<div>
-					<label>Sort by:</label>
-					<select onChange={SortByInputHandler}>
+					<label className="mr-2">Sort by:</label>
+					<select className="p-2 rounded-l" onChange={SortByInputHandler}>
 						<option value="make">Make</option>
 						<option value="year">Year</option>
+						<option value="displacement">Displacement</option>
+						<option value="power">Power</option>
+						<option value="torque">Torque</option>
+						<option value="weight">Weight</option>
+						<option value="price">Price</option>
+						<option value="rating">Rating</option>
+						<option value="acceleration">Acceleration</option>
+						<option value="top-speed">Top speed</option>
+						<option value="handling">Handling</option>
+						<option value="braking-power">Braking power</option>
+					</select>
+					<select className="p-2 rounded-r" onChange={SortOrderInputHandler}>
+						<option value="accending">Accending</option>
+						<option value="descending">Descending</option>
 					</select>
 				</div>
 			</div>
-			<BikeGallery displayData={displayData} />
+			<BikeGallery displayData={displayData} sortKey={sortKey} />
 		</Layout>
 	);
 }
